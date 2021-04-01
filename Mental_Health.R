8295211793b79018952222c1c25b44a155b24863
Jinhao Lu <jxl469@psu.edu>
2021-04-01 16:44:26 -0400

    Update Mental_Health.R

--- Mental_Health.R
+++ Mental_Health.R
@@ -54,8 +54,8 @@ test$Dec_20 <- confirmed_data[,345]
 test$Jan_21 <- confirmed_data[,376]
 test$Feb_21 <- confirmed_data[,403]
 
-a <- test[,2:15]
-num_confirmed <- as.data.frame(t(apply(a,2,sum)))
+c <- test[,2:15]
+num_confirmed <- as.data.frame(t(apply(c,2,sum)))
 
 #calculate the difference by months
 diff <- num_confirmed[,1:(ncol(num_confirmed)-1)]
@@ -94,8 +94,8 @@ test$Dec_20 <- deaths_data[,345]
 test$Jan_21 <- deaths_data[,376]
 test$Feb_21 <- deaths_data[,403]
 
-a <- test[,2:15]
-num_deaths <- as.data.frame(t(apply(a,2,sum)))
+c <- test[,2:15]
+num_deaths <- as.data.frame(t(apply(c,2,sum)))
 
 #calculate the difference between by months
 diff <- num_deaths[,1:(ncol(num_deaths)-1)]
@@ -109,7 +109,7 @@ day_num_diff <- deaths_data - day_diff
 
 deaths_day_num_diff <- as.data.frame(t(apply(day_num_diff,2,sum)))
 
-#====================================================================================
+#=================================infection rate by states===================================
 #infection rate of New York
 NewYork <- confirmed_19[Province_State == 'New York']
 NewYork_confirmed <- as.data.frame(NewYork$Province_State)
@@ -136,7 +136,7 @@ NewYork_confirmed <- as.data.frame(t(apply(c,2,sum)))
 #Population estimates of New york 2019 = 19453561 https://www.census.gov/quickfacts/NY
 pop <- 19453561
 #infection rate
-NewYork_rate <- NewYork_confirmed/pop
+NewYork_infection_rate <- NewYork_confirmed/pop
 
 
 #California
@@ -165,7 +165,7 @@ California_confirmed <- as.data.frame(t(apply(c,2,sum)))
 #Population estimates of California = 39512223 https://www.census.gov/quickfacts/fact/table/CA/PST045219
 pop <- 39512223
 #infection rate
-California_rate <- California_confirmed/pop
+California_infection_rate <- California_confirmed/pop
 
 #Florida
 Florida <- confirmed_19[Province_State == 'Florida']
@@ -193,4 +193,115 @@ Florida_confirmed <- as.data.frame(t(apply(c,2,sum)))
 #Population estimates of Florida = 21477737  https://www.census.gov/quickfacts/FL
 pop <- 21477737
 #infection rate
-Florida_rate <- Florida_confirmed/pop
+Florida_infection_rate <- Florida_confirmed/pop
+
+
+#=================================deaths rate by states===================================
+#deaths rate of New York
+NewYork <- deaths_19[Province_State == 'New York']
+NewYork_deaths <- as.data.frame(NewYork$Province_State)
+pop <- sum(NewYork$Population)# 19453561
+NewYork <- NewYork[,13:415]
+
+#NewYork_deaths number by month 
+NewYork_deaths$Jan_20 <- NewYork[,10]
+NewYork_deaths$Feb_20 <- NewYork[,39]
+NewYork_deaths$Mar_20 <- NewYork[,70]
+NewYork_deaths$Apr_20 <- NewYork[,100]
+NewYork_deaths$May_20 <- NewYork[,131]
+NewYork_deaths$Jun_20 <- NewYork[,161]
+NewYork_deaths$Jul_20 <- NewYork[,192]
+NewYork_deaths$Aug_20 <- NewYork[,223]
+NewYork_deaths$Sep_20 <- NewYork[,253]
+NewYork_deaths$Oct_20 <- NewYork[,284]
+NewYork_deaths$Nov_20 <- NewYork[,314]
+NewYork_deaths$Dec_20 <- NewYork[,345]
+NewYork_deaths$Jan_21 <- NewYork[,376]
+NewYork_deaths$Feb_21 <- NewYork[,403]
+
+c <- NewYork_deaths[,2:15]
+NewYork_deaths <- as.data.frame(t(apply(c,2,sum)))
+#Population estimates of New york 2019 = 19453561 https://www.census.gov/quickfacts/NY
+#infection rate
+NewYork_deaths_rate <- NewYork_deaths/pop
+
+
+#California
+California <- deaths_19[Province_State == 'California']
+California_deaths <- as.data.frame(California$Province_State)
+pop <- sum(California$Population) #39512223
+California <- California[,13:415]
+
+#California_deaths number by month 
+California_deaths$Jan_20 <- California[,10]
+California_deaths$Feb_20 <- California[,39]
+California_deaths$Mar_20 <- California[,70]
+California_deaths$Apr_20 <- California[,100]
+California_deaths$May_20 <- California[,131]
+California_deaths$Jun_20 <- California[,161]
+California_deaths$Jul_20 <- California[,192]
+California_deaths$Aug_20 <- California[,223]
+California_deaths$Sep_20 <- California[,253]
+California_deaths$Oct_20 <- California[,284]
+California_deaths$Nov_20 <- California[,314]
+California_deaths$Dec_20 <- California[,345]
+California_deaths$Jan_21 <- California[,376]
+California_deaths$Feb_21 <- California[,403]
+
+c <- California_deaths[,2:15]
+California_deaths <- as.data.frame(t(apply(c,2,sum)))
+#Population estimates of California = 39512223 https://www.census.gov/quickfacts/fact/table/CA/PST045219
+#infection rate
+California_deaths_rate <- California_deaths/pop
+
+#Florida
+Florida <- deaths_19[Province_State == 'Florida']
+Florida_deaths <- as.data.frame(Florida$Province_State)
+pop <- sum(Florida$Population) #21477737
+Florida <- Florida[,13:415]
+
+#Florida_deaths number by month 
+Florida_deaths$Jan_20 <- Florida[,10]
+Florida_deaths$Feb_20 <- Florida[,39]
+Florida_deaths$Mar_20 <- Florida[,70]
+Florida_deaths$Apr_20 <- Florida[,100]
+Florida_deaths$May_20 <- Florida[,131]
+Florida_deaths$Jun_20 <- Florida[,161]
+Florida_deaths$Jul_20 <- Florida[,192]
+Florida_deaths$Aug_20 <- Florida[,223]
+Florida_deaths$Sep_20 <- Florida[,253]
+Florida_deaths$Oct_20 <- Florida[,284]
+Florida_deaths$Nov_20 <- Florida[,314]
+Florida_deaths$Dec_20 <- Florida[,345]
+Florida_deaths$Jan_21 <- Florida[,376]
+Florida_deaths$Feb_21 <- Florida[,403]
+
+c <- Florida_deaths[,2:15]
+Florida_deaths <- as.data.frame(t(apply(c,2,sum)))
+#Population estimates of Florida = 21477737  https://www.census.gov/quickfacts/FL
+#infection rate
+Florida_deaths_rate <- Florida_deaths/pop
+
+state <- t(list('New York','California','Florida'))
+state <- as.data.frame(state)
+NewYork_infection_rate <- cbind(state$V1, NewYork_infection_rate)
+California_infection_rate <- cbind(state$V2, California_infection_rate)
+Florida_infection_rate <- cbind(state$V3, Florida_infection_rate)
+
+NewYork_deaths_rate <- cbind(state$V1, NewYork_deaths_rate)
+California_deaths_rate <- cbind(state$V2, California_deaths_rate)
+Florida_deaths_rate <- cbind(state$V3, Florida_deaths_rate)
+
+setnames(NewYork_infection_rate,'"New York"',"state")
+setnames(California_infection_rate,'"California"',"state")
+setnames(Florida_infection_rate,'"Florida"',"state")
+
+setnames(NewYork_deaths_rate,'"New York"',"state")
+setnames(California_deaths_rate,'"California"',"state")
+setnames(Florida_deaths_rate,'"Florida"',"state")
+
+infection_rate <- rbind(NewYork_infection_rate,California_infection_rate,Florida_infection_rate)
+
+deaths_rate <- rbind(NewYork_deaths_rate, California_deaths_rate, Florida_deaths_rate)
+
+
